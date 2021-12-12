b1077108ee3b85b775657817aca714b5be396466
jursonovicst <github.com@jursonovics.com>
2021-12-12 21:40:23 +0100

    add a simple example, unittest can be run from pycharm...

--- main.py
+++ main.py
@@ -100,4 +100,21 @@ class TestCaches(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    # define objects
+    x = Obj('x', 1000, 300)
+    a = Obj('a', 100, 300)
+    b = Obj('b', 100, 300)
+    c = Obj('c', 100, 300)
+    d = Obj('d', 30, 300)
+
+    # create cache
+    cache = ProtectedFIFOCache(400)
+
+    # place requests
+    cache.recv(0, a)
+    cache.recv(1, b)
+    cache.recv(2, a)
+    cache.recv(3, d)
+    cache.recv(3.1, d)
+    cache.recv(3.2, d)
+    cache.recv(1000, d)
