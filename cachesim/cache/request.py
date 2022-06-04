69144dd980caab3ddd825ebd012053e6e57dd82b
jursonovicst <github.com@jursonovics.com>
2022-06-04 10:36:34 +0200

    Clean

---
+++ cachesim/cache/request.py
@@ -0,0 +1,88 @@
+class Request:
+    """
+    Represents the metadata of the cache request
+    """
+
+    def __init__(self, time: float, hash: str, size: int, maxage: int):
+        """
+
+        :param time: Timestamp (unix time) at the request was placed.
+        :param hash: Uniq identifier of the requested object (hash).
+        :param size: Size of the requested object.
+        :param maxage: Maximum caching time. For non cacheable objects, use a non positive value.
+        """
+        # metadata to be known before fetch
+        self._time = time
+        self._hash = hash
+
+        # metadata only known after fetch
+        self._size = size
+        self._maxage = maxage
+        self._enter = None  # time at the object entered the cache (for TTL calculation)
+
+        # misc
+        self._fetched = False  # object fetched (retrieved from origin)
+
+    @property
+    def time(self) -> float:
+        return self._time
+
+    @property
+    def hash(self) -> str:
+        return self._hash
+
+    @property
+    def size(self) -> int:
+        assert self.fetched, f"size property cannot be known before object fetch!"
+        return self._size
+
+    @property
+    def maxage(self) -> int:
+        assert self.fetched, f"maxage property cannot be known before object fetch!"
+        return self._maxage
+
+    @property
+    def cacheable(self) -> bool:
+        assert self.fetched, f"cacheable cannot be known before object fetch!"
+        return self.maxage > 0
+
+    @property
+    def enter(self) -> float:
+        assert self.fetched, f"enter cannot be known before object fetch!"
+        return self._enter
+
+    @enter.setter
+    def enter(self, time: float):
+        assert self.fetched, f"enter cannot be set before object fetch!"
+        self._enter = time
+
+    def isexpired(self, now: float) -> bool:
+        assert self.enter is not None, f"Object must first enter the cache to determine if it is expired."
+        return self.enter + self.maxage < now
+
+    @property
+    def fetched(self) -> bool:
+        return self._fetched
+
+    @fetched.setter
+    def fetched(self, val: bool):
+        self._fetched = val
+
+    def __add__(self, other):
+        """For sum() function"""
+        assert isinstance(other, Request), f"Operator add has been implemented only for {self.__class__.__name__} type."
+        return self.size + other
+
+    def __radd__(self, other):
+        """For sum() function"""
+        assert isinstance(other, int), f"Operator radd has been implemented only for {self.__class__.__name__} type."
+        return other + self.size
+
+    def __eq__(self, other):
+        """To implement in operator"""
+        assert isinstance(other, Request), f"Operator EQ has been implemented only for {self.__class__.__name__} type."
+        return self.hash == other.hash
+
+    def __str__(self):
+        """For logging"""
+        return f"{self._time} {self.hash} {self.size if self.fetched else '-'} {self.maxage if self.fetched else '-'}"
---
+++ cachesim/cache/test_request.py
@@ -0,0 +1,116 @@
+import random
+import string
+import time
+from unittest import TestCase
+
+from cachesim.cache import Request
+
+
+class TestRequest(TestCase):
+
+    def test_request(self):
+        for i in range(1000):
+
+            ts = time.time() + random.random() * 2 * 60 * 60 * 24 * 365 + 60 * 60 * 24 * 365
+            letters = string.ascii_lowercase
+            hash = ''.join(random.choice(letters) for i in range(random.randint(5, 20)))
+            size = random.randint(1024, 1024 * 1024 * 100)
+            maxage = random.randint(0, 30)
+            request = Request(time=ts, hash=hash, size=size, maxage=maxage)
+
+            # before fetch
+            self.assertEqual(ts, request.time)
+            self.assertEqual(hash, request.hash)
+            self.assertFalse(request.fetched)
+            self.assertEqual(f"{ts} {hash} - -", str(request))
+
+            with self.assertRaises(AssertionError):
+                a = request.size
+            with self.assertRaises(AssertionError):
+                a = request.maxage
+            with self.assertRaises(AssertionError):
+                a = request.cacheable
+            with self.assertRaises(AssertionError):
+                a = request.enter
+            with self.assertRaises(AssertionError):
+                a = request.isexpired(ts + random.randint(0, 35))
+
+            # after fetch
+            request.fetched = True
+            self.assertEqual(ts, request.time)
+            self.assertEqual(hash, request.hash)
+            self.assertTrue(request.fetched)
+            self.assertEqual(f"{ts} {hash} {size} {maxage}", str(request))
+
+            self.assertEqual(size, request.size)
+            self.assertEqual(maxage, request.maxage)
+            if maxage == 0:
+                self.assertFalse(request.cacheable)
+            else:
+                self.assertTrue(request.cacheable)
+            self.assertIsNone(request.enter)
+            with self.assertRaises(AssertionError):
+                a = request.isexpired(ts + random.randint(0, 35))
+
+            # after cache enter
+            request.enter = ts
+            self.assertEqual(ts, request.time)
+            self.assertEqual(hash, request.hash)
+            self.assertTrue(request.fetched)
+
+            self.assertEqual(size, request.size)
+            self.assertEqual(maxage, request.maxage)
+            if maxage == 0:
+                self.assertFalse(request.cacheable)
+            else:
+                self.assertTrue(request.cacheable)
+            self.assertEqual(ts, request.enter)
+            delta = random.randint(0, 35)
+            if delta > maxage:
+                self.assertTrue(request.isexpired(ts + delta))
+            else:
+                self.assertFalse(request.isexpired(ts + delta))
+
+    def test_add(self):
+        for i in range(1000):
+            letters = string.ascii_lowercase
+
+            ts = time.time() + random.random() * 2 * 60 * 60 * 24 * 365 + 60 * 60 * 24 * 365
+            index = ''.join(random.choice(letters) for i in range(random.randint(5, 20)))
+            size_a = random.randint(1024, 1024 * 1024 * 100)
+            maxage = random.randint(0, 30)
+            request_a = Request(time=ts, hash=index, size=size_a, maxage=maxage)
+
+            ts = time.time() + random.random() * 2 * 60 * 60 * 24 * 365 + 60 * 60 * 24 * 365
+            index = ''.join(random.choice(letters) for i in range(random.randint(5, 20)))
+            size_b = random.randint(1024, 1024 * 1024 * 100)
+            maxage = random.randint(0, 30)
+            request_b = Request(time=ts, hash=index, size=size_b, maxage=maxage)
+
+            request_a.fetched = True
+            request_b.fetched = True
+            self.assertEqual(size_a + size_b, request_a + request_b)
+
+    def test_eq(self):
+        for i in range(1000):
+            letters = string.ascii_lowercase
+            index_a = ''.join(random.choice(letters) for i in range(random.randint(5, 20)))
+
+            ts = time.time() + random.random() * 2 * 60 * 60 * 24 * 365 + 60 * 60 * 24 * 365
+            size = random.randint(1024, 1024 * 1024 * 100)
+            maxage = random.randint(0, 30)
+            request_a1 = Request(time=ts, hash=index_a, size=size, maxage=maxage)
+
+            ts = time.time() + random.random() * 2 * 60 * 60 * 24 * 365 + 60 * 60 * 24 * 365
+            size = random.randint(1024, 1024 * 1024 * 100)
+            maxage = random.randint(0, 30)
+            request_a2 = Request(time=ts, hash=index_a, size=size, maxage=maxage)
+
+            ts = time.time() + random.random() * 2 * 60 * 60 * 24 * 365 + 60 * 60 * 24 * 365
+            index_b = ''.join(random.choice(letters) for i in range(random.randint(5, 20)))
+            size = random.randint(1024, 1024 * 1024 * 100)
+            maxage = random.randint(0, 30)
+            request_b = Request(time=ts, hash=index_b, size=size, maxage=maxage)
+
+            self.assertEqual(request_a1, request_a2)
+            self.assertNotEqual(request_a1, request_b)
