b9cb57e735fd0a82fd0f3ec3d3bd40468764bd7b
jursonovicst <github.com@jursonovics.com>
2022-06-05 10:42:08 +0200

    update to the bare cache implementation

---
+++ example/cache/noncache.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from cachesim.cache import Cache, Request
+
+
+class NonCache(Cache):
+    """
+    Very basic example of a cache, which actually does not cache at all.
+    """
+
+    def __init__(self):
+        super().__init__(size=0)
+
+    def _lookup(self, requested: Request) -> Optional[Request]:
+        # object is never in cache
+        return None
+
+    def _admit(self, fetched: Request) -> bool:
+        # never allow object entering the cache
+        return False
+
+    def _store(self, fetched: Request):
+        pass
+
+    def _evict(self):
+        pass
+
+    @property
+    def treshold(self) -> bool:
+        # no need to eviction
+        return False
