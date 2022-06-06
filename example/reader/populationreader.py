41dedc7a178daa82700e0b85a33e82918a77e643
jursonovicst <github.com@jursonovics.com>
2022-06-06 22:35:17 +0200

    clean

--- example/reader/populationreader.py
+++ example/reader/populationreader.py
@@ -18,26 +18,12 @@ class PopulationReader(Reader):
         assert all(w >= 0 for w in weights), f"Weights should be non negative integers"
         self._weights = weights
 
-#        self._counter = 0
         self._requests = None
 
     def __iter__(self):
         self._requests = iter(random.choices(self._population, self._weights, k=self.totalcount))
 
         return super().__iter__()
-
-#    def __iter__(self):
-#        self._counter = self.totalcount
-#        return super().__iter__()
-
-    # def __next__(self) -> Request:
-    #     if self._counter == 0:
-    #         raise StopIteration
-    #
-    #     self._counter -= 1
-    #     chosen = random.choices(self._population, self._weights, k=1)[0]
-    #
-    #     return Request(time=time.time(), hash=chosen._hash, size=chosen._size, maxage=chosen._maxage)
 
     def __next__(self):
         chosen = next(self._requests)
