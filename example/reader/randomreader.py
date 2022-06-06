ac2fdfc8e0d901a9a3a05be19d264e80cc6015ce
jursonovicst <github.com@jursonovics.com>
2022-06-06 11:47:37 +0200

    add random reader

---
+++ example/reader/randomreader.py
@@ -0,0 +1,34 @@
+import random
+import time
+
+from cachesim import Reader
+from cachesim.cache import Request
+
+
+class RandomReader(Reader):
+
+    def __init__(self, totalsize: int, hashlen: int = 8, sizegen: callable = random.randint(1, 100),
+                 maxagegen: callable = random.randint(60, 300)):
+        assert totalsize >= 0, f"I expect a non negative totalsize, got '{totalsize}'"
+        self._totalsize = int(totalsize)
+        assert hashlen > 0, f"I expect a positive hashlen, got '{hashlen}'"
+        self._hashlen = int(hashlen)
+        self._sizegen = sizegen
+        self._maxagegen = maxagegen
+        self._counter = 0
+
+    @property
+    def totalsize(self) -> int:
+        return self._totalsize
+
+    def __iter__(self):
+        self._counter = self.totalsize
+        return super().__iter__()
+
+    def __next__(self) -> Request:
+        if self._counter == 0:
+            raise StopIteration
+
+        self._counter -= 1
+        return Request(time.time(), '%x' % random.getrandbits(self._hashlen * 8), int(self._sizegen),
+                       int(self._maxagegen))
