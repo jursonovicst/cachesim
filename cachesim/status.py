fd9a7e6c067358254d69c0e5fea268a0f0aca9e3
jursonovicst <github.com@jursonovics.com>
2023-10-13 11:58:55 +0200

    clean

--- cachesim/status.py
+++ cachesim/status.py
@@ -5,6 +5,6 @@ class Status(Enum):
     """
     Cache status type.
     """
-    HIT = 'hit'  # object returned from caches
-    MISS = 'miss'  # object not in caches, fetched from origin
-    PASS = 'pass'  # forced caches bypass (object too big or caches admission denied it)
+    HIT = 'hit'  # object served from cache
+    MISS = 'miss'  # object not in cache, fetched from origins
+    PASS = 'pass'  # forced cache bypass (object too big or cache admission denied)
